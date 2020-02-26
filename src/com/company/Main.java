2f400e603ce3e8ecea41140c630b33596bc762ec
Nicolae Trascaian <60103768+TrascaianNicolae98@users.noreply.github.com>
2020-02-26 19:45:46 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("hello world");
 	System.out.println("hello world 2");
-        System.out.println("merge");
+        System.out.println("merge2");
 
     }
 }
